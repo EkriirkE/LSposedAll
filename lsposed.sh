1864bf7c05de5cad70bc710e2ec3669b6d988d41
Erik Johnson <github@ekriirke.net>
2025-03-11 22:55:24 +0100

    Update lsposed.sh
    Put module list in more convenient spot

--- lsposed.sh
+++ lsposed.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+#Modules to enable in 'a','b','c' format
+mod="'fi.veetipaananen.android.disableflagsecure2','undercover.hide.mock','eu.faircode.xlua','taco.scoop','com.github.thepiemonster.hidemocklocation','com.android1500.gpssetter'"
+
+#Whitelist in 'a','b','c' format, prevent & remove mods from this app (ex. mods on gms causes integrity to fail, and george crashes if any mod is applied)
+wl="'com.google.android.gms','at.erstebank.george'"
+
 #Userid list, apply to app in these profiles (0 is main, 10 and 11 are work profile and another profile on my phone)
 #Either specify space-delimited, or every profile as detected in /data/user
 #ul="0 10 11"
 ul=$(ls /data/user)
 
-#Whitelist in 'a','b','c' format, prevent & remove mods from this app (ex. mods on gms causes integrity to fail, and george crashes if any mod is applied)
-wl="'com.google.android.gms','at.erstebank.george'"
-
-
 db=/data/adb/lspd/config/modules_config.db
-i=$(sqlite3 $db "SELECT mid FROM modules WHERE module_pkg_name IN ('fi.veetipaananen.android.disableflagsecure2','undercover.hide.mock','eu.faircode.xlua','taco.scoop','com.github.thepiemonster.hidemocklocation','com.android1500.gpssetter');")
+i=$(sqlite3 $db "SELECT mid FROM modules WHERE module_pkg_name IN ($mod);")
 pm list packages | sort | while read p; do
 	p=${p:8}
 	[[ $wl == *"'$p'"* ]] && continue
